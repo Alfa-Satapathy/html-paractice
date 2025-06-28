b8e1a10c0050d875d16409f8a12cd8bb3973e2e9
Piyush Ranjan Satapathy <109914906+Alfa-Satapathy@users.noreply.github.com>
2025-06-28 11:17:00 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,48 @@
+export const allMovies = [
+  {
+    id: 1,
+    movieName: "Golmal",
+    price: 100,
+    imageUrl: "https://picsum.photos/id/237/200/300",
+  },
+  {
+    id: 2,
+    movieName: "3idiots",
+    price: 200,
+    imageUrl: "https://picsum.photos/id/237/200/300",
+  },
+  {
+    id: 3,
+    movieName: "JabWeMet",
+    price: 300,
+    imageUrl: "https://picsum.photos/id/237/200/300",
+  },
+  {
+    id: 4,
+    movieName: "Bahubali",
+    price: 400,
+    imageUrl: "https://picsum.photos/id/237/200/300",
+  },
+  {
+    id: 5,
+    movieName: "Tiranga",
+    price: 500,
+    imageUrl: "https://picsum.photos/id/237/200/300",
+  },
+];
+
+export const movieOptions = [
+  { label: "Golmal", value: "Golmal" },
+  { label: "3idiots", value: "3idiots" },
+  { label: "JabWeMet", value: "JabWeMet" },
+  { label: "Bahubali", value: "Bahubali" },
+  { label: "Tiranga", value: "Tiranga" },
+];
+
+export const moviePrices = [
+  { label: "Golmal", value: 100 },
+  { label: "3idiots", value: 200 },
+  { label: "JabWeMet", value: 300 },
+  { label: "Bahubali", value: 400 },
+  { label: "Tiranga", value: 500 },
+];
